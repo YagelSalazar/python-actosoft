ca62b5d71ad5f49c4ea298fc307280062032cf49
YagelSalazar <yagels.xd@gmail.com>
2020-04-07 11:44:39 -0500

    diezmo

--- loops.py
+++ loops.py
@@ -35,6 +35,7 @@ students = [
 ]
 
 for student in students:
+  print('*************************************')
   print('Nombre del alumno:', student[0])
   print('Calificaciones')
 
@@ -44,10 +45,51 @@ for student in students:
   # Mostrar cada una
   # Obtener el promedio
   # Si su promedio es mayor que 6, imprimir en la consola 'APROBADO', sino, imprimir 'REPROBADO'
+  # sum() Toma una lista de números y hace la suma de todos
 
-  calif = (notes[0] + notes[1] + notes[2])/3
-  print('el alumno ', student[0], ' sacó ', calif)
-  if calif >= 6:
-    print('aprobado')
-  else:
-    print('reprobado')
+  for i, note in enumerate(notes): #enumerate() permite traer el valor y el index de el elemento de una lista
+    print(f'Calificación #{i + 1}: {note}')
+  
+  average = sum(notes)/len(notes)
+  # Operador Ternario if else
+  # print('PROMEDIO:', average)
+  # if average > 6:
+  #   print('APROBADO')
+  # else:
+  #   print('REPROBADO')
+  print(f'PROMEDIO: {average:.2f} - {"APROBADO" if average > 6 else "REPROBADO"}')
+
+print("===============================")
+print("USANDO EL WHILE")
+
+# Script to greet people. The program will stop when the user says S, if the option entered for the user is invalid, we have a nested while.
+opt = 0
+while opt != 'S':
+  name = input("Por tu nombre pa saludarte, compa: ")
+  print("Hola!", name)
+  exit = input("Quieres salirte alv? (S/N): ")
+  while exit != "S" and exit != "N":
+    print("No mames, ingresa una opción válida")
+    exit = input("Quieres salirte alv? (S/N): ")
+  opt = exit
+print("Gracias por dejame saludaaarte")
+
+print('Vamos a la iglesia')
+print('Me das tu diezmo?')
+
+acum = 0
+payments = 0
+
+while payments < 10:
+  pay = float(input('Ingresa tu diezmo a la iglesia: '))
+  acum += pay
+  payments += 1
+print(f'Total de lo que le has pagado a la Iglesia: {acum:.2f}')
+
+
+newAc = 0
+
+for 1 in range(0, 10):
+  pay = float(input('Ingresa tu diezmo a la iglesia: '))
+  payments += newAc
+print(f'Total de lo que le has pagado a la Iglesia: {newAc:.2f}')
